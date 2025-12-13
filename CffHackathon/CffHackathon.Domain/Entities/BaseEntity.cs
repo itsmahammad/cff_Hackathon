de188d6e5bf6a2fbe257ae63b0e399b53f43f8f8
Mahammad Mammadov <Mahammad Mammadov@DESKTOP-BRCKLEU>
2025-12-13 10:53:03 +0400

    baseEntity

---
+++ CffHackathon/CffHackathon.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CffHackathon.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+
+        public bool IsActive { get; set; } = true;
+    }
+}
