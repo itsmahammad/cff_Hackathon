f3f3521aabed4cc084b5398a41652f5102938182
Mahammad Mammadov <Mahammad Mammadov@DESKTOP-BRCKLEU>
2025-12-13 11:14:04 +0400

    added another entities

---
+++ CffHackathon/CffHackathon.Domain/Entities/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CffHackathon.Domain.Entities
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+
+        // Navigation
+        public ICollection<MenuItem> MenuItems { get; set; } = new List<MenuItem>();
+    }
+
+}
---
+++ CffHackathon/CffHackathon.Domain/Entities/MenuItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CffHackathon.Domain.Entities
+{
+    public class MenuItem : BaseEntity
+    {
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+        public string? PhotoUrl { get; set; }
+
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+
+        public bool IsAvailable { get; set; } = true;
+        public Category Category { get; set; } = null!;
+
+        public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+    }
+
+}
