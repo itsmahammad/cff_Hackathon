157fd30c4a53b3fed7c7ae8a1b1a87bd6dd97d09
Mahammad Mammadov <Mahammad Mammadov@DESKTOP-BRCKLEU>
2025-12-13 13:05:48 +0400

    added categoryDto and Service

---
+++ CffHackathon/CffHackathon.Application/Common/Models/Reservation/ReservationAddDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CffHackathon.Application.Common.Models.Reservation
+{
+    internal class ReservationAddDto
+    {
+        public int UserId { get; set; }
+        public int RoomId { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
---
+++ CffHackathon/CffHackathon.Application/Common/Models/Reservation/ReservationInfoDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CffHackathon.Application.Common.Models.Reservation
+{
+    public class ReservationInfoDto
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public int TableId { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
+        public string Status { get; set; } = null!;
+
+    }
+}
