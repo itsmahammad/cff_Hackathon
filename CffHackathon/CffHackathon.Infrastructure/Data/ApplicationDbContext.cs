228685b11ec2f05386d2a3cd2be4a73f34530806
Mahammad Mammadov <Mahammad Mammadov@DESKTOP-BRCKLEU>
2025-12-13 11:36:15 +0400

    added dbsets

--- CffHackathon/CffHackathon.Infrastructure/Data/ApplicationDbContext.cs
+++ CffHackathon/CffHackathon.Infrastructure/Data/ApplicationDbContext.cs
@@ -5,7 +5,14 @@ namespace CffHackathon.Infrastructure.Data
     public class ApplicationDbContext(DbContextOptions options) : DbContext(options), IApplicationDbContext
     {
 
-        public DbSet<Cate> SampleEntities { get; set; }
+        public DbSet<Category> Categories => Set<Category>();
+        public DbSet<MenuItem> MenuItems => Set<MenuItem>();
+        public DbSet<Order> Orders => Set<Order>();
+        public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+        public DbSet<Table> Tables => Set<Table>();
+        public DbSet<Payment> Payments => Set<Payment>();
+        public DbSet<Reservation> Reservations => Set<Reservation>();
+
 
 
 
