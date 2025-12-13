a7a34485e783f06c0f16e7f5d69a9ca61286cdaa
fidanst-azmpa201-dev <fidanst-azmpa201@code.edu.az>
2025-12-13 14:49:07 +0400

    Added OrderService

---
+++ CffHackathon/CffHackathon.Application/Common/Models/Table/TableInOrdersDto.cs
@@ -0,0 +1,16 @@
+﻿using CffHackathon.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CffHackathon.Application.Common.Models.Table
+{
+    public class TableInOrdersDto
+    {
+        public string Number { get; set; }
+        public int Capacity { get; set; }
+        public TableStatus Status { get; set; }
+    }
+}
